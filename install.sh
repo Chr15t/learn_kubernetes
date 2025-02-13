8969f00c443135fb1cded5c564e6f8c1a7c2fa16
christian <mamychristian22@gmail.com>
2025-02-13 21:05:32 +0300

    chore(install): configure sysctl settings, add Docker's GPG key and repository, install containerd, generate default configuration

--- install.sh
+++ install.sh
@@ -10,11 +10,40 @@ overlay
 br_netfilter
 EOF
 
-
+# Configure sysctl settings for Kubernetes networking
 cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-iptables = 1
 net.bridge.bridge-nf-call-ip6tables = 1
 net.ipv4.ip_forward = 1
 EOF
 
+# Apply sysctl settings
 sudo sysctl --system
+
+# Add Docker's official GPG key and repository
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Update package index
+sudo apt-get update
+
+# Install containerd
+sudo apt-get install -y containerd.io
+
+# Generate default containerd configuration file
+sudo containerd config default | sudo tee /etc/containerd/config.toml 
+
+# Edit the configuration file to set `SystemdCgroup = true`
+# Path: /etc/containerd/config.toml
+
+
+# restart service 
+systemctl restart containerd
