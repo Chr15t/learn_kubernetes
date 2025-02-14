0e46b6fab9d1e09aecde0d9b198003a8b6f26c15
christian <mamychristian22@gmail.com>
2025-02-14 04:27:44 +0300

    feat(installation-script): update installation script with Kubernetes package installation

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 # Load the overlay module
 sudo modprobe overlay
 
@@ -47,3 +48,18 @@ sudo containerd config default | sudo tee /etc/containerd/config.toml
 
 # restart service 
 systemctl restart containerd
+
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.32/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+
+
+# This overwrites any existing configuration in /etc/apt/sources.list.d/kubernetes.list
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.32/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+
+
+sudo systemctl enable kubelet.service
+sudo systemctl enable containerd.service
